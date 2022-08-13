b7446950bbd2a12a148d9d9ff1c5c41b5911517d
goodcat46 <goodcat1994@gmail.com>
2022-08-13 11:57:31 +0300

    1.002
    + зробив 10 слайдерів із стандартними стилями

--- js/index.js
+++ js/index.js
@@ -1,8 +1,100 @@
+// ================================ SLIDER__TEST_1
 $(document).ready(function () {
-  $('.fruits-slider').slick({
-    infinite: false,
-    slidesToShow: 1,
+  // тут вказується клас DIVчика яз якого потрібно зробити слайдер
+  $('.slider__test_1').slick({
+    infinite: true,
+    slidesToShow: 6,
     slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+  });
+});
+// ================================ SLIDER__TEST_2
+$(document).ready(function () {
+  $('.slider__test_2').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+  });
+});
+// ================================ SLIDER__TEST_3
+$(document).ready(function () {
+  $('.slider__test_3').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+  });
+});
+// ================================ SLIDER__TEST_4
+$(document).ready(function () {
+  $('.slider__test_4').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+  });
+});
+// ================================ SLIDER__TEST_5
+$(document).ready(function () {
+  $('.slider__test_5').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+  });
+});
+// ================================ SLIDER__TEST_6
+$(document).ready(function () {
+  $('.slider__test_6').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+  });
+});
+// ================================ SLIDER__TEST_7
+$(document).ready(function () {
+  $('.slider__test_7').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+  });
+});
+// ================================ SLIDER__TEST_8
+$(document).ready(function () {
+  $('.slider__test_8').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+  });
+});
+// ================================ SLIDER__TEST_9
+$(document).ready(function () {
+  $('.slider__test_9').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+  });
+});
+// ================================ SLIDER__TEST_10
+$(document).ready(function () {
+  $('.slider__test_10').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 3,
     prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
     nextArrow: '<button type="button" class="slick-next">&#62;</button>',
   });
