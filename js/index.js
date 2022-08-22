0693155f9da55ff1d1082d18073bcfa1fa40c90e
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2022-08-22 23:40:24 +0300

    1.002

--- js/index.js
+++ js/index.js
@@ -1,101 +1,385 @@
-// ================================ SLIDER__TEST_1
+// ================================ SLIDER__TEST_1 true,
 $(document).ready(function () {
   // тут вказується клас DIVчика яз якого потрібно зробити слайдер
   $('.slider__test_1').slick({
     infinite: true,
+    dots: true,
     slidesToShow: 6,
-    slidesToScroll: 1,
-    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
-    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+    // prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    // nextArrow: '<button type="button" class="slick-next">&#62;</button>',
   });
 });
 // ================================ SLIDER__TEST_2
 $(document).ready(function () {
   $('.slider__test_2').slick({
     infinite: true,
+    dots: true,
     slidesToShow: 6,
     slidesToScroll: 3,
-    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
-    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+    // prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    // nextArrow: '<button type="button" class="slick-next">&#62;</button>',
   });
 });
 // ================================ SLIDER__TEST_3
 $(document).ready(function () {
   $('.slider__test_3').slick({
     infinite: true,
+    dots: true,
     slidesToShow: 6,
     slidesToScroll: 3,
-    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
-    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      //
+      //
+      //
+    ],
+    // prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    // nextArrow: '<button type="button" class="slick-next">&#62;</button>',
   });
 });
 // ================================ SLIDER__TEST_4
 $(document).ready(function () {
   $('.slider__test_4').slick({
     infinite: true,
+    dots: true,
     slidesToShow: 6,
     slidesToScroll: 3,
-    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
-    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      //
+      //
+      //
+    ],
+    // prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    // nextArrow: '<button type="button" class="slick-next">&#62;</button>',
   });
 });
 // ================================ SLIDER__TEST_5
 $(document).ready(function () {
   $('.slider__test_5').slick({
     infinite: true,
+    dots: true,
     slidesToShow: 6,
     slidesToScroll: 3,
-    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
-    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      //
+      //
+      //
+    ],
+    // prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    // nextArrow: '<button type="button" class="slick-next">&#62;</button>',
   });
 });
 // ================================ SLIDER__TEST_6
 $(document).ready(function () {
   $('.slider__test_6').slick({
     infinite: true,
+    dots: true,
     slidesToShow: 6,
     slidesToScroll: 3,
-    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
-    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      //
+      //
+      //
+    ],
+    // prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    // nextArrow: '<button type="button" class="slick-next">&#62;</button>',
   });
 });
 // ================================ SLIDER__TEST_7
 $(document).ready(function () {
   $('.slider__test_7').slick({
     infinite: true,
+    dots: true,
     slidesToShow: 6,
     slidesToScroll: 3,
-    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
-    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      //
+      //
+      //
+    ],
+    // prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    // nextArrow: '<button type="button" class="slick-next">&#62;</button>',
   });
 });
 // ================================ SLIDER__TEST_8
 $(document).ready(function () {
   $('.slider__test_8').slick({
     infinite: true,
+    dots: true,
     slidesToShow: 6,
     slidesToScroll: 3,
-    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
-    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      //
+      //
+      //
+    ],
+    // prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    // nextArrow: '<button type="button" class="slick-next">&#62;</button>',
   });
 });
 // ================================ SLIDER__TEST_9
 $(document).ready(function () {
   $('.slider__test_9').slick({
     infinite: true,
+    dots: true,
     slidesToShow: 6,
     slidesToScroll: 3,
-    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
-    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      //
+      //
+      //
+    ],
+    // prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    // nextArrow: '<button type="button" class="slick-next">&#62;</button>',
   });
 });
 // ================================ SLIDER__TEST_10
 $(document).ready(function () {
   $('.slider__test_10').slick({
     infinite: true,
+    dots: true,
     slidesToShow: 6,
     slidesToScroll: 3,
-    prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
-    nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      //
+      //
+      //
+    ],
+    // prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+    // nextArrow: '<button type="button" class="slick-next">&#62;</button>',
   });
 });
